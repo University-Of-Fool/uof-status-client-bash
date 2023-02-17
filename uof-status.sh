2ec5d038b9cec93d3175a6333579d0d659d0a783
UrsusFeline <ursusfeline07@gmail.com>
2023-02-17 19:24:21 +0800

    Update uof-status.sh
    还没有完成

--- uof-status.sh
+++ uof-status.sh
@@ -8,20 +8,20 @@
 
 ##### Config_START #####
 
-# 回报服务器状态的间隔，以秒为单位
-_interval=60  
+# 回报服务器状态的间隔,请加上合适的单位(一般不需要更改)
+_INTERVAL=60s
 
-# 回报状态类型（只在特殊情况下修改）
-_status=True
+# 回报状态类型（不需要更改）
+_STATUS=True
 
-# token
-_token=""
+# Token (会自动生成)
+_TOKEN=
 
-# 客户端 ID
-_id=1
+# 客户端 ID (会自动生成)
+_ID=
 
 # status 服务器 api 地址
-_serverIP="http://127.0.0.1:4044/"
+_SERVER_IP="http://127.0.0.1:4044"
 
 ##### Config_END #####
 
@@ -41,13 +41,21 @@ $*
 
 # 获取服务器列表
 GET_LIST() {
-curl -X GET "$_serverIP/api/server/get"
+curl -X GET "$_SERVER_IP/api/server/get"
 }
 # 获取状态信息
 GET_STAT() {
-curl -X GET "$_serverIP/api/status/get/$_id"
+curl -X GET "$_SERVER_IP/api/status/get/$_id"
 }
 # 检测主服务器状态,遇到错误请注释
 GET_LIST || echo -e "\033[31mERROT:Can't connect to main server.\033[0m" & exit 1
 # 新增服务器
 if [[ $1 == "-i" || $1 == "--init" ]];then
+curl -X POST $_SERVER_IP/api/server/put << EOF
+
+{
+    "token":"$2",
+    "name": "$3",
+    "description": "$4"
+}
+EOF
